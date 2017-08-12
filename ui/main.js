97c4d7f75b3e9f36490a3d55c4b05c4964f78bdb
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-12 23:37:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,16 @@ var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
 
 var img = document.getElementById("madi");
+
+var marginLeft = 0;
+function moveRight()
+{
+    marginLeft = marginLeft  + 10;
+    madi.style.marginLeft = marginLeft + 'px';
+}
+
 madi.onclick = function()
 {
-    madi.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
+    
 };
