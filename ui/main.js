c234d59786e56060236209a1dd3cbc809c0d791c
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-13 09:03:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,31 @@
-console.log('Loaded!');
+// console.log('Loaded!');
 
 
 
-var element = document.getElementById("main-text");
+// var element = document.getElementById("main-text");
 
-element.innerHTML = "New Value";
+// element.innerHTML = "New Value";
 
-var img = document.getElementById("madi");
+// var img = document.getElementById("madi");
 
-var marginLeft = 0;
-function moveRight()
-{
-    marginLeft = marginLeft  + 1;
-    madi.style.marginLeft = marginLeft + 'px';
-}
+// var marginLeft = 0;
+// function moveRight()
+// {
+//     marginLeft = marginLeft  + 1;
+//     madi.style.marginLeft = marginLeft + 'px';
+// }
 
-madi.onclick = function()
-{
-    var interval = setInterval(moveRight,50);
+// madi.onclick = function()
+// {
+//     var interval = setInterval(moveRight,50);
     
+// };
+
+
+var button = getElementById('counter');
+button.onClick = function()
+{
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString;
 };
