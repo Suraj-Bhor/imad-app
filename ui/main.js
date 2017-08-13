49107a385fd23635fa25955a77cee229a7ad27b9
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-13 09:04:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,10 +22,11 @@
 // };
 
 
-var button = getElementById('counter');
+var button = document.getElementById('counter');
+var counter = 0;
 button.onClick = function()
 {
     counter = counter + 1;
     var span = document.getElementById('count');
-    span.innerHTML = counter.toString;
+    span.innerHTML = counter.toString();
 };
