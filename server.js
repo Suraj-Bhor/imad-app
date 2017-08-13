708a6c01d32800144f433fde3e5ac8a06c81ea4f
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-13 09:57:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,6 +152,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+
+var names=[];
 app.get('/submit-name/:name',function(req,res){
    
    var name = req.params.name;
