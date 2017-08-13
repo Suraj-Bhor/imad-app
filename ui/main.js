855b14380a58795e51cf947c31fc4c3bb265fc0c
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-13 09:32:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ button.onclick = function()
     var request = new XMLHttpRequest();
     request.onreadystatechange = function()
     {
-        if(request.readyState === XMLHtttpRequest.DONE)
+        if(request.readyState === XMLHttpRequest.DONE)
         {
             if(request.status === 200)
             {
