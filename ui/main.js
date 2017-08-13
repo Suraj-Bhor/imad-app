14047bd047a76f1e5ae37b59f4694640faed3596
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-13 10:27:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,4 @@
-// console.log('Loaded!');
 
-
-
-// var element = document.getElementById("main-text");
-
-// element.innerHTML = "New Value";
-
-// var img = document.getElementById("madi");
-
-// var marginLeft = 0;
-// function moveRight()
-// {
-//     marginLeft = marginLeft  + 1;
-//     madi.style.marginLeft = marginLeft + 'px';
-// }
-
-// madi.onclick = function()
-// {
-//     var interval = setInterval(moveRight,50);
-    
-// };
 
 
 var button = document.getElementById('counter');
@@ -45,27 +24,38 @@ button.onclick = function()
     request.send(null);
     
 };
- 
+
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
-    var names = request.responseText;
-    names = JSON.parse(names);
-    var list = '';
-    for(var i=0;i<names.length;i++)
+        var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
     {
-        list += '<li>' + names[i] + '</li>';
-        
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var names = request.responseText;
+                names = JSON.parse(names); 
+                var list = '';
+                for(var i=0;i<names.length;i++)
+                {
+                    list += '<li>' + names[i] + '</li>';
+                    
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+     };
+    
+  
+    request.open('GET','http://surajbhor4.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
     
 };
-
-
-
-
-
-
-
 
