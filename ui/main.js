77ba14208b7c7a3edfa9af2b73f321a052bfb9f7
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-13 09:27:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,10 +23,22 @@
 
 
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function()
 {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHtttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://surajbhor4.imad.hasura.io/counter',true);
+    request.send(null);
+    
 };
