58fc62f45dd0a163c7edcfd5f53bf00c97484360
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-13 10:28:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,7 +52,8 @@ submit.onclick = function()
         }
      };
     
-  
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://surajbhor4.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     
