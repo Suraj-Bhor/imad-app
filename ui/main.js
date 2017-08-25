3e045bb3c3baa0c5eff1c6cc31d76c208d3951eb
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-25 15:22:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,5 @@
 
 
-
-var button = document.getElementById('counter');
-button.onclick = function()
-{
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function()
-    {
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status === 200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://surajbhor4.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
-    
-};
-
-
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
@@ -38,24 +10,28 @@ submit.onclick = function()
         {
             if(request.status === 200)
             {
-                var names = request.responseText;
-                names = JSON.parse(names); 
-                var list = '';
-                for(var i=0;i<names.length;i++)
-                {
-                    list += '<li>' + names[i] + '</li>';
-                    
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
+                console.log('User logged in');
+                alert('logged in successfully');
+            }
+            else if (request.status === 403)
+            {
+                alert('username/password incorrect');
+            }
+            else if (request.status === 500)
+            {
+                alert('Something went wrong on the server side');   
             }
         }
      };
     
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('POST','http://surajbhor4.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://surajbhor4.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
     
 };
