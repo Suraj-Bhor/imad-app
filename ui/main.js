4a74ae425048e6ed33f2d3ded3b55cfb408457b3
Suraj-Bhor <suraj.bhor4@gmail.com>
2017-08-25 13:47:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,7 +54,7 @@ submit.onclick = function()
     
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET','http://surajbhor4.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('POST','http://surajbhor4.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     
     
